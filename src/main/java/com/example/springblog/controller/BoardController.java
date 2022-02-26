5b13b1b6dafd0bf997a25df117775c54de1971ad
sosow0212 <sosow0212@naver.com>
2022-02-26 19:01:26 +0900

    글 목록 불러오기 th:each 성공

--- src/main/java/com/example/springblog/controller/BoardController.java
+++ src/main/java/com/example/springblog/controller/BoardController.java
@@ -1,15 +1,20 @@
 package com.example.springblog.controller;
 
-import com.example.springblog.config.auth.PrincipalDetail;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.example.springblog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping({"", "/"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("boards", boardService.글목록());
         return "index";
     }
 
--- src/main/java/com/example/springblog/service/BoardService.java
+++ src/main/java/com/example/springblog/service/BoardService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 
 @Service
 public class BoardService {
@@ -21,6 +23,10 @@ public class BoardService {
         board.setUser(user);
 
         boardRepository.save(board);
+    }
+
+    public List<Board> 글목록() {
+        return boardRepository.findAll();
     }
 
 }
