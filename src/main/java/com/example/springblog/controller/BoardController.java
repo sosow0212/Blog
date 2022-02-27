0d4c10bd6147687a8c4c90694fa1aca5bc903e6e
sosow0212 <sosow0212@naver.com>
2022-02-27 13:43:48 +0900

    글 상세보기 처리, th:utext로 html태그까지 렌더링

--- src/main/java/com/example/springblog/controller/BoardController.java
+++ src/main/java/com/example/springblog/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Controller
@@ -21,6 +22,13 @@ public class BoardController {
         model.addAttribute("boards", boardService.글목록(pageable));
         return "index";
     }
+
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.글상세보기(id));
+        return "board/detail";
+    }
+
 
     @GetMapping("/board/saveForm")
     public String saveForm() {
--- src/main/java/com/example/springblog/service/BoardService.java
+++ src/main/java/com/example/springblog/service/BoardService.java
@@ -31,4 +31,11 @@ public class BoardService {
         return boardRepository.findAll(pageable);
     }
 
+    public Board 글상세보기(int id) {
+        return boardRepository.findById(id)
+                .orElseThrow(() -> {
+                    return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
+                });
+    }
+
 }
