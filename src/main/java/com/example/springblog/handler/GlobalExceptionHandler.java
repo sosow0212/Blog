3f64d5424b089d9b0a2be56b8f7aba91d208119f
sosow0212 <sosow0212@naver.com>
2022-03-07 12:20:35 +0900

    Error Exception 처리 - 페이징 완성

--- src/main/java/com/example/springblog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/springblog/handler/GlobalExceptionHandler.java
@@ -1,19 +1,27 @@
-package com.example.springblog.handler;
-
-import com.example.springblog.dto.ResponseDto;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@ControllerAdvice // ?
-@RestController
-public class GlobalExceptionHandler {
+/**
+ *
+ * 전체 잘못된 요청 처리하는 코드
 
-    @ExceptionHandler(value = Exception.class)
-    private ResponseDto<String> handleArgumentException(Exception e) {
-        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
-        // INTERNAL_SERVER_ERROR = 500 값임
-    }
-}
+ package com.example.springblog.handler;
+
+ import com.example.springblog.dto.ResponseDto;
+ import org.springframework.http.HttpStatus;
+ import org.springframework.web.bind.annotation.ControllerAdvice;
+ import org.springframework.web.bind.annotation.ExceptionHandler;
+ import org.springframework.web.bind.annotation.RestController;
+
+
+ @ControllerAdvice // ?
+ @RestController
+ public class GlobalExceptionHandler {
+
+ @ExceptionHandler(value = Exception.class)
+ private ResponseDto<String> handleArgumentException(Exception e) {
+ return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+ // INTERNAL_SERVER_ERROR = 500 값임
+ }
+ }
+
+ */
